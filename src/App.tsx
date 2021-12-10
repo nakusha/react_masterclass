dd1b976d136c562b2c5f9ce02f06654e636c77d1
JYS <runnway@naver.com>
2021-12-10 16:22:11 +0900

    typescript form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="tomato" borderColor="teal" />
-      <Circle bgColor="teal" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+          value={value}
+        />
+        <button>log in</button>
+      </form>
     </div>
   );
 };
