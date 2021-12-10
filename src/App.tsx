721373e5d5d02dbd6be224da09d4d2d1ec429185
JYS <runnway@naver.com>
2021-12-10 15:48:37 +0900

    type script props practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,11 @@
-import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 30px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 50px;
-    }
-  }
-`;
-
-// const Input = styled.input.attrs({ required: true, minLength: 10 })`
-//   background-color: tomato;
-// `;
+import Circle from "./Circle";
 
 const App = () => {
   return (
-    <Wrapper>
-      <Title>안녕하세요</Title>
-      <Box>
-        <Emoji as="p">😁</Emoji>
-      </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="tomato" borderColor="teal" />
+      <Circle bgColor="teal" />
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  backgroundColor: "whitesmoke",
-  textColor: "#111",
-};
-
-const light = false;
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={light ? lightTheme : darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
